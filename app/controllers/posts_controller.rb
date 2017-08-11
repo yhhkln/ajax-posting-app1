f666043f36d03907815b29c22f3acdf843cbc443
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-11 23:48:36 +0800

    star

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		sleep(1)
 		@post = Post.find(params[:id])
 		@post.update!( post_params )
 
@@ -61,6 +62,20 @@ class PostsController < ApplicationController
 		@post.save!
 
 		render :json => { :message => "ok", :flag_at => @post.flag_at, :id => @post.id}
+	end
+
+	def rate
+		@post = Post.find(params[:id])
+
+		existing_score = @post.find_score(current_user)
+		if existing_score
+			existing_score.update( :score => params[:score] )
+		else
+			@post.scores.create( :score => params[:score], :user => current_user )
+		end
+
+		render :json => { :average_score => @post.average_score }
+
 	end
 
 	protected
